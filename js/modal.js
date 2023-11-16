47b0753a0c215c36eb9ae19cbd4a12c847ab4f2c
YehorK88 <141238245+YehorK88@users.noreply.github.com>
2023-11-16 22:46:54 +0200

    mobile menu left

--- js/modal.js
+++ js/modal.js
@@ -3,9 +3,10 @@
     openModalBtn: document.querySelector('[data-modal-open]'),    
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),  
-};
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+    };
+    
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
 
     function toggleModal() {    
     refs.modal.classList.toggle('is-hidden');
